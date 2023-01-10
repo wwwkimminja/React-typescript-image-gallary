acd3113d1a09d20f4b5c280e87433a0f8bbb62db
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-01-10 23:24:32 +0900

    Image render

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ImageBox from './components/ImageBox';
 
@@ -11,19 +10,36 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='initial-box'>
+      <div className={`gallery-box ${imageList.length>0 ? 'row':""}`}>
         {
           imageList.length===0&&<div className='text-center'>No Image <br/> Add Image</div>
         }
         <input type="file" ref={inputRef} onChange={(event)=>{
-          const value=event.currentTarget.value
-          if(value) setImageList((prev)=>[...prev,value])
+         
+          if(event.currentTarget.files?.[0]){
+            const file=event.currentTarget.files[0]
+            const reader=new FileReader();
+            reader.readAsDataURL(file)
+            reader.onload=(event)=>{
+              setImageList((prev)=>[...prev,event.target?.result as string])
+            }
+           
+          } 
+         
         }}/>
+ 
+        <>
+    {
+        imageList.map((el,idx) =>{
+          return <ImageBox key={el+idx} src={el}/>
+        })
+      }
+        </>
         <div className='plus-box' onClick={()=>{
           inputRef.current?.click()
         }}>+</div>
       </div>
-
+  
     </div>
   );
 }
