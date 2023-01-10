b5bb26c9d064d83a0a7b76c2142f4d669629829d
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-01-10 22:51:08 +0900

    feat : input image file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageBox from './components/ImageBox';
 
 function App() {
+
+  const inputRef=useRef<HTMLInputElement>(null);
+
+  const [imageList,setImageList]=useState<string[]>([])
+
   return (
     <div className='container'>
       <div className='initial-box'>
-        <div className='text-center'>No Image <br/> Add Image
-        </div>
-        <div className='plus-box'>+</div>
+        {
+          imageList.length===0&&<div className='text-center'>No Image <br/> Add Image</div>
+        }
+        <input type="file" ref={inputRef} onChange={(event)=>{
+          const value=event.currentTarget.value
+          if(value) setImageList((prev)=>[...prev,value])
+        }}/>
+        <div className='plus-box' onClick={()=>{
+          inputRef.current?.click()
+        }}>+</div>
       </div>
+
     </div>
   );
 }
